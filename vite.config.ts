8beea8461dfdb0227c483141a2c0bd6870483aaa
jlgsjlgs <jonathanlimgs@hotmail.com>
2025-10-10 14:36:46 +0800

    misc: change vite config
    Signed-off-by: jlgsjlgs <jonathanlimgs@hotmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  base: '/portfolio/', 
+  plugins: [react(), tailwindcss()],
+  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   server: {
-    port:3000,
+    port: 3000,
   },
 })
